d70e8780fda8bc279e0acfd9238ac777e7a03dca
Fikirte Mulugeta <fikirliya@gmail.com>
2020-05-18 00:04:14 -0700

    index.js, data.js, views/detail.handlebars, views/home.handlebars

--- data.js
+++ data.js
@@ -6,15 +6,56 @@ let musics = [
 {title: "umbrella", artist:"Rihanna, JAY-Z",type:"R & B",year:2007},
 {title: "I hope", artist:"Gabby Barrett", type:"country", year:2019},
 {title: "find my way", artist:"da baby", type:"hip hop", year:2020},
+
 ];
 
 exports.getAll = () => {
 return musics;
 };
 
-/*exports.getmusic = (title) => {
-    return musics.find((music) => {
-        return music.title === title;
-    })
-}*/
+//here begins assignment 3
+//get Item
+// console.log(musics.length);
+
+// const getItem = (title) =>{
+//     return musics.find((music) => {
+//         return music.title === title;
+//     });
+// }
+
+// let found = getItem('energy')
+// console.log(found)
+
+exports.get = (title) => {
+    return musics.find((item) => {
+        return item.title.toLowerCase() === title.toLowerCase();
+    });
+};
+//add item
+
+exports.delete = (title) => {
+    // retain array length for later comparison after array modification
+    const oldLength = musics.length;
+    musics = musics.filter((item) => {
+        return item.title !== title;
+    });
+    // if old & new array lengths differ, item was deleted
+    return {deleted: oldLength !== musics.length, total: musics.length };
+};
+
+exports.add = (newMusic) => {
+    const oldLength = musics.length;
+    // use existing get() method to check if book already in our list
+    let found = this.get(newMusic.title);
+    if (!found) {
+        musics.push(newMusic);
+    }
+    // if old & new array lengths differ, item was added
+    return {added: oldLength !== musics.length, total: musics.length };
+};
+
+
+
+
+
 
--- index.js
+++ index.js
@@ -10,37 +10,34 @@ const exphbs = require("express-handlebars"); // should be at top of module
 //initialize express server 
 const app = express();
 
-//get data
 const all = musics.getAll();
 
-//Configure application
+
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
-//set location for static files
-app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.engine('handlebars', exphbs({defaultLayout: false}));
 app.set("view engine", "handlebars");
 
-
-//send static file as response
 app.get('home.html',(request, response) => {
 response.type('text/html');
 response.sendFile(__dirname + '/public/home.html');
 });
 
-//send content of 'home' view as HTML response
+
 app.get('/', (request, response) =>{
 response.render('home', {musics: all});
 });
 
-// send content of 'home' view
+
 app.get('/detail', (request,response) => {
 let index = request.query.index;
-let musics = all[index];
-response.render('detail', {index:index, musics: musics});
+let music = all[index];
+response.render('detail', {index:index, music: music});
 });
 
-//send plain text response
+
 app.get('/about',(request,response) => {
 response.type('text/plain');
 response.send('About page');
