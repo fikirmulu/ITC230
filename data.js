33068748e74934188ac50c0575b0fe2128e85841
Fikirte Mulugeta <fikirliya@gmail.com>
2020-06-20 20:23:04 -0700

    data.js. index.js, views/home_react.handlebars

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 'use strict'
 
 let musics = [
-{title: "myohmy", artist:"Camila Cabelo", type:"pop", year:2019},
-{title: "energy", artist:"Drake", type:"rap",year:2015},
-{title: "umbrella", artist:"Rihanna, JAY-Z",type:"R & B",year:2007},
-{title: "I hope", artist:"Gabby Barrett", type:"country", year:2019},
-{title: "find my way", artist:"da baby", type:"hip hop", year:2020},
+{title: "myohmy", artist:"Camila Cabelo", genre:"pop", year:2019},
+{title: "energy", artist:"Drake", type:"rap",genre:2015},
+{title: "umbrella", artist:"Rihanna, JAY-Z",genre:"R & B",year:2007},
+{title: "I hope", artist:"Gabby Barrett", genre:"country", year:2019},
+{title: "find my way", artist:"da baby", genre:"hip hop", year:2020},
 
 ];
 
